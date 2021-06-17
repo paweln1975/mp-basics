6b5666025686ccdea2a1c11b54f6a3b1fe864f2f
paweln1975 <pawel.niedziela@gmail.com>
2021-06-17 09:06:46 +0200

    string slicing

--- basics/variables.py
+++ basics/variables.py
@@ -24,6 +24,7 @@ for i in range(0, len(parrot)):
 
 print()
 
+# with negative index we go back
 for i in range(0, len(parrot)):
     print(parrot[-i-1], end=' ')
 
@@ -34,4 +35,40 @@ print("Slice 1: " + parrot[0:9])
 print("Slice 2: " + parrot[:9])
 print("Slice 3: " + parrot[10:])
 print("Slice 4: " + parrot[:])
-print("Slice 5: " + parrot[::2]) # with step
+print("Slice 5: " + parrot[::2])    # with step
+
+numbers = "1,2,3;4,5,6 7,8,9"
+print(numbers[0::2])
+
+separators = numbers[1::2]
+print(separators)
+values = "".join(char if char not in separators else " " for char in numbers).split()
+print([int(val) for val in values])
+
+# negative step
+letters = "abcdefghijklmnopqrstuvwxyz"
+backwards = letters[25::-1]
+print(backwards)
+
+# slicing idiom (1st Python idiom)
+print(letters[::-1])
+
+# produces qpo
+print(letters[-10:-13:-1])
+print(letters[16:13:-1])
+
+# produces edcba
+print(letters[-22::-1])
+print(letters[4::-1])
+
+# produces 8 last characters in reverse order
+print(letters[:-9:-1])
+
+# last 4 letters
+print(letters[-4:])
+
+# first char
+print(letters[:1])
+
+# last char
+print(letters[-1:])

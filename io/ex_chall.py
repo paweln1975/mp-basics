8ffdbec54225458ba63fa893992273bff680037d
paweln1975 <pawel.niedziela@gmail.com>
2021-11-24 09:43:30 +0100

    io chall

---
+++ io/ex_chall.py
@@ -0,0 +1,5 @@
+with open("/home/centos/IdeaProjects/mp-basics/io/output.txt", "a") as output_file:
+    for i in range(2, 13):
+        for j in range(1, 13):
+            print("{0:>2} times {1:>2} is {2}".format(i, j,  i * j), file=output_file)
+        print("-" * 20, file=output_file)

b962b7fde263202454a903ff219f9c1605523d5c
paweln1975 <pawel.niedziela@gmail.com>
2021-11-25 09:13:50 +0100

    binary start

---
+++ io/io_binary.py
@@ -0,0 +1,25 @@
+with open("/home/centos/IdeaProjects/mp-basics/binary", "bw") as bin_file:
+    bin_file.write(bytes(range(1, 17)))
+
+with open("/home/centos/IdeaProjects/mp-basics/binary", "br") as bin_file:
+    for b in bin_file:
+        print(b)
+
+a = 65534
+b = 2993839
+
+with open("/home/centos/IdeaProjects/mp-basics/binary", "bw") as bin_file:
+    bin_file.write(a.to_bytes(2, 'big'))
+    bin_file.write(b.to_bytes(4, 'big'))
+    bin_file.write(b.to_bytes(4, 'little'))
+
+with open("/home/centos/IdeaProjects/mp-basics/binary", "br") as bin_file:
+    c = int.from_bytes(bin_file.read(2), 'big')
+    print(c)
+    d = int.from_bytes(bin_file.read(4), 'big')
+    print(d)
+    e = int.from_bytes(bin_file.read(4), 'little')
+    print(e)
+
+
+

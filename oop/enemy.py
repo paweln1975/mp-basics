5e8f1fe9ba3c792dc428bdd7d0198305895c9519
paweln1975 <pawel.niedziela@gmail.com>
2021-12-23 22:43:10 +0100

    inheritance

--- oop/enemy.py
+++ oop/enemy.py
@@ -1,4 +1,4 @@
-class Player(object):
+class Enemy(object):
 
     def __init__(self, name: str, level: int = 1):
         self.name = name
@@ -6,10 +6,10 @@ class Player(object):
         if level <= 0:
             raise ValueError("Level must be positive!")
         self._level = level
-        self._score = 1000 * level
+        self._damage = 100 * level
 
     def __str__(self):
-        return "Name: {0.name} Lives: {0.lives} Level: {0.level} Score: {0.score}".format(self)
+        return "Name: {0.name} Lives: {0.lives} Level: {0.level} Score: {0.damage}".format(self)
 
     def _get_lives(self):
         return self._lives
@@ -25,38 +25,29 @@ class Player(object):
 
     def _set_level(self, level):
         if level >= 0:
-            self._score += (level-self._level) * 1000
+            self._damage += (level-self._level) * 500
             self._level = level
         else:
             raise ValueError("Level cannot be negative or zero!")
 
-    def increase_level(self):
-        self.level += 1
-
-    def decrease_level(self):
-        self.level -= 1
-
     @property
-    def score(self):
-        return self._score
+    def damage(self):
+        return self._damage
 
-    @score.setter
-    def score(self, score):
-        self._score = score
+    @damage.setter
+    def damage(self, score):
+        self._damage = score
 
     lives = property(_get_lives, _set_lives, doc="Number of lives")
     level = property(_get_level, _set_level, doc="Level value")
     # data attribute cannot have the same name as property
 
 
-p = Player("Tim")
-p.lives = 10
-print(p)
-p.increase_level()
-print(p)
-p.level = 5
-print(p)
-p.level = 3
-print(p)
-p.decrease_level()
+class Troll(Enemy):
+    pass
 
+
+t = Troll("Troll")
+print(t)
+e = Enemy("Boris")
+print(e)

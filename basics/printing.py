7df7eb70d55fa062149c2abe30a241ea88a493ac
paweln1975 <pawel.niedziela@gmail.com>
2023-09-23 12:55:50 +0200

    hyberskill 2

--- basics/printing.py
+++ basics/printing.py
@@ -1,3 +1,5 @@
+import datetime
+
 print('Hello world!')
 print(1+2)
 print(6*7)
@@ -9,7 +11,7 @@ print("Test \"Escape character\"")
 print('Test 1\tTest 2\tTest 3\tTest 4')
 
 print("Text \
-splitted \
+split \
 over \
 3 \
 lines")
@@ -20,3 +22,11 @@ print("* "*4)
 for x in range(1, 3):
     print("*     *")
 print("* "*4)
+
+# function that print names of a day
+
+
+d = datetime.date(2023, 9, 24)
+for i in range(1, 8):
+    print(d.strftime("%A"))
+    d = d + datetime.timedelta(days=1)

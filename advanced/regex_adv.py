0867cc4bf65bc4180d2949df7ba722144d8ea58a
paweln1975 <pawel.niedziela@gmail.com>
2024-11-20 23:29:23 +0100

    regexp cont. adv 2

--- advanced/regex_adv.py
+++ advanced/regex_adv.py
@@ -18,8 +18,39 @@ Regular expression more functions
 >>> text = anonimize('@some_twitter_user I love dogs, @another_twitter_user!')
 >>> print(text)
 <AUTHOR> I love dogs, <HANDLE>!
+
+>>> string = 'JetBrains Academy'
+>>> template_pos = 'JetBrains (?=Academy)'
+>>> template_neg = 'JetBrains (?!Academy)'
+>>> regexp_match(template_pos, string)
+True
+
+>>> regexp_match(template_neg, string)
+False
+
+>>> string = 'JetBrains Academy'
+>>> template_pos = '(<=JetBrains )Academy'
+>>> template_neg = '(<!JetBrains )Academy'
+
+>>> regexp_search(template_neg, string)
+False
+
+>>> print(calculate_sum('2 meals $20, groceries $120, 1 month rent $500'))
+This week you have spent: 660 dollars
+
+>>> print(calculate_sum('5 meals $1, groceries $30, 1 month rent $100, 5 tickets to ride $20'))
+This week you have spent: 235 dollars
+
+>>> string = '<li>Sister</li> <li>Father</li> <li>Mother-in-law</li>'
+>>> print(extract_html_li(string))
+Sister
+Father
+Mother-in-law
+None
 """
 import re
+from re import template
+
 
 def regexp_find_all(template, string) -> list:
     match = re.findall(template, string)
@@ -40,3 +71,23 @@ def anonimize(string) -> str:
     template = '@[\w_]*'
     new_text = re.sub(template, other_text, new_text)
     return new_text
+
+def regexp_match(template, string) -> bool:
+    return re.match(template, string) is not None
+
+def regexp_search(template, string) -> bool:
+    return re.search(template, string) is not None
+
+def calculate_sum(string: str) -> str:
+    template = ' ?(\d*)? [\w\s]+ \$(\d+),?'
+    groups = re.findall(template, string)
+    result = 0
+    for c, value in groups:
+        result += int(c) * int(value) if c is not '' else int(value)
+    return f'This week you have spent: {result} dollars'
+
+def extract_html_li(string):
+    template = '(?<=<li>)(.*?)(?=<\/li>)'
+    groups = re.findall(template, string)
+    for group in groups:
+        print(group)
